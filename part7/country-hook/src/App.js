cc6a034045bc5f114ed6804fa501116d253bbdef
name <email>
2023-01-20 16:04:26 -0500

    part 7f start

---
+++ part7/country-hook/src/App.js
@@ -0,0 +1,106 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    console.log("effect!");
+    const getCountry = async () => {
+      try {
+        const baseUrl = "https://restcountries.com/v3.1/name/";
+        const response = await axios.get(`${baseUrl}/${name}?fullText=true`);
+        const data = await response.data;
+        setCountry({
+          name: {
+            common: data[0].name.common,
+            official: data[0].name.official,
+          },
+          population: data[0].population,
+          capital: data[0].capital[0],
+          flags: data[0].flags,
+        });
+        console.log("data :>> ", data);
+        console.log("country :>> ", country);
+      } catch (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+        setCountry(null);
+      }
+    };
+    getCountry();
+  }, [name]);
+
+  return country;
+};
+
+const Country = ({ country }) => {
+  if (!country) {
+    return <div>not found...</div>;
+  }
+
+  return (
+    <div>
+      <h3>{country.name.common}</h3>
+      <div>population {country.population}</div>
+      <div>capital {country.capital}</div>
+      <img
+        src={country.flags.png}
+        height="100"
+        alt={`flag of ${country.name.common}`}
+      />
+    </div>
+  );
+};
+
+const App = () => {
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
+
+  const fetch = (e) => {
+    e.preventDefault();
+    setName(nameInput.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  );
+};
+
+export default App;
---
+++ part7/routed-anecdotes/src/components/Notification.js
@@ -0,0 +1,23 @@
+function Notification({ notification }) {
+  if (notification === null) {
+    return null;
+  }
+
+  const style = {
+    color: notification.type === "alert" ? "red" : "green",
+    background: "lightgrey",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  };
+
+  return (
+    <div id="notification" style={style}>
+      {notification}
+    </div>
+  );
+}
+
+export default Notification;
